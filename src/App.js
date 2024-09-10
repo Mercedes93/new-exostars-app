bd05cefa84932ac03f6608060d98b7fe6ce88dea
Mercedes93 <101393916+Mercedes93@users.noreply.github.com>
2024-09-10 20:03:28 +0200

    framework

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to ExoStars</h1>
+        <p>
+          Explore the stars from the perspective of distant exoplanets!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
